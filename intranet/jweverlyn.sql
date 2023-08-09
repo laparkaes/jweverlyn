df16949d20d8e6d92f6a941b365f6682857ce97a
G-Desktop <laparkaes@gmail.com>
2023-08-09 09:27:13 -0500

    .

--- intranet/jweverlyn.sql
+++ intranet/jweverlyn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 23-08-06 18:41
+-- 생성 시간: 23-08-09 09:26
 -- 서버 버전: 10.4.24-MariaDB
 -- PHP 버전: 7.4.29
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `access`
+--
+
+CREATE TABLE `access` (
+  `access_id` int(11) NOT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  `action` varchar(30) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `valid` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `account`
 --
 
@@ -33,9 +47,29 @@ CREATE TABLE `account` (
   `username` varchar(200) NOT NULL,
   `password` varchar(250) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `valid` tinyint(1) NOT NULL DEFAULT 1,
   `last_logged_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `registed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 테이블의 덤프 데이터 `account`
+--
+
+INSERT INTO `account` (`account_id`, `role_id`, `username`, `password`, `name`, `valid`, `last_logged_at`, `updated_at`, `registed_at`) VALUES
+(1, 1, 'laparkaes@gmail.com', '$2y$10$GgXbbw1zKLysaytroxNpl.Vp3EsLKplcnKr6H/6xqZBimt7ENGnf6', 'Georgio Park', 1, NULL, '2023-08-07 01:21:19', '2023-08-07 01:21:19');
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `module`
+--
+
+CREATE TABLE `module` (
+  `module_id` int(11) NOT NULL,
+  `module` varchar(50) NOT NULL,
+  `valid` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -62,11 +96,24 @@ INSERT INTO `role` (`role_id`, `role`) VALUES
 --
 
 --
+-- 테이블의 인덱스 `access`
+--
+ALTER TABLE `access`
+  ADD PRIMARY KEY (`access_id`),
+  ADD KEY `fk_access_module` (`module_id`);
+
+--
 -- 테이블의 인덱스 `account`
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`),
-  ADD KEY `account_role` (`role_id`);
+  ADD KEY `fk_account_role` (`role_id`) USING BTREE;
+
+--
+-- 테이블의 인덱스 `module`
+--
+ALTER TABLE `module`
+  ADD PRIMARY KEY (`module_id`);
 
 --
 -- 테이블의 인덱스 `role`
@@ -79,10 +126,22 @@ ALTER TABLE `role`
 --
 
 --
+-- 테이블의 AUTO_INCREMENT `access`
+--
+ALTER TABLE `access`
+  MODIFY `access_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- 테이블의 AUTO_INCREMENT `account`
 --
 ALTER TABLE `account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 테이블의 AUTO_INCREMENT `module`
+--
+ALTER TABLE `module`
+  MODIFY `module_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- 테이블의 AUTO_INCREMENT `role`
@@ -93,6 +152,12 @@ ALTER TABLE `role`
 --
 -- 덤프된 테이블의 제약사항
 --
+
+--
+-- 테이블의 제약사항 `access`
+--
+ALTER TABLE `access`
+  ADD CONSTRAINT `fk_access_module` FOREIGN KEY (`module_id`) REFERENCES `module` (`module_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- 테이블의 제약사항 `account`
